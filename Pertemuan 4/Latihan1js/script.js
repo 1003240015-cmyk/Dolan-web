56b198c4447ab6c592c5bf2347f83b8470e2ab46
Farel Aliffian F <1003240015@students.itts.ac.id>
2025-10-21 09:57:16 +0700

    Add files via upload

---
+++ Pertemuan 4/Latihan1js/script.js
@@ -0,0 +1,5 @@
+function tampilkanNama() {
+      const depan = document.getElementById('namaDepan').value;
+      const belakang = document.getElementById('namaBelakang').value;
+      console.log(`Nama Lengkap : ${depan} ${belakang}`);
+    }
